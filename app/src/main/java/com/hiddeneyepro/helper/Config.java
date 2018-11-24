a93dc26872d091f005789867dec5a8f633a8a6fc
Yash Paragkumar Shah <ypshah.13@gmail.com>
2018-11-24 02:43:37 -0800

    Notification and App icons added!

--- app/src/main/java/com/hiddeneyepro/helper/Config.java
+++ app/src/main/java/com/hiddeneyepro/helper/Config.java
@@ -7,7 +7,6 @@ public class Config {
 
     public static final String APP_NAME = "HiddenEye";
     public static final String SHARED_PREF_NAME = "HiddenEyePref";
-    //public static final String REST_IP_ADDRESS = "http://192.168.0.6:8080";
     public static final String REST_URL = "http://192.168.0.6:8080/HiddenEye/rest/DBConnection";
 
     public static final String APP_FOLDER_NAME= "HiddenEyePro13";
